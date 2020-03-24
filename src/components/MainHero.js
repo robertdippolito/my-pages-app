0a657d244026eaddc4135c18554c30d962ef0a31
Robert D'Ippolito <robertdippolito@Roberts-MacBook-Pro-2.local>
2020-03-23 21:09:05 -0400

    removed boilerplate

---
+++ src/components/MainHero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MainContainer extends React.Component {
+    render(){
+        return (
+            <div>
+               <p>Hello World</p> 
+            </div>
+        )
+    }
+}
+
+export default MainContainer
